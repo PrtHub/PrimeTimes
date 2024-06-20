dd4973998066fe79101cbeaa2776de8ccdf7603e
Pritam <pri10145@gmail.com>
2024-06-20 12:37:45 +0530

    remove the tsx and ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import './index.css'
 import { Provider } from 'react-redux'
-import store from './lib/store.ts'
+import store from './lib/store'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
     <App />
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
